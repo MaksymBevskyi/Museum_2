7aeba0be9c7bccf09e9f3e029047a18b24e59b57
Maksym Bevskyi <bevskiymaks@gmail.com>
2023-12-25 19:29:03 +0100

    make build

---
+++ index.71cce126.js
@@ -0,0 +1,3 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});var e=document.querySelector("#form-reload");e.addEventListener("submit",function(n){n.preventDefault(),e.reset()});//# sourceMappingURL=index.71cce126.js.map
+
+//# sourceMappingURL=index.71cce126.js.map
